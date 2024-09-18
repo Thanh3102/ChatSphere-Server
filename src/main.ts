f30e4edc886a17b9f173c5916a20c719cb94d44e
Thanh3102 <thanhduong200602@gmail.com>
2024-09-18 12:29:06 +0700

    Change CORS

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,10 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: [process.env.CLIENT_BASE_URL],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
   });
 
   app.useGlobalPipes(
